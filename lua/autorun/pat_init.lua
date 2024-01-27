4848ebec9b05b8abd04d255eb8bed8fbc3a8d9db
shockpast <shockpast@gmail.com>
2024-01-27 23:03:29 +0300

    fix: corrected order of adding functions into SPatches table

--- lua/autorun/pat_init.lua
+++ lua/autorun/pat_init.lua
@@ -3,20 +3,20 @@ SPatches = SPatches or {}
 local color_cornflower = Color(84, 109, 229)
 local color_gray = Color(210, 218, 226)
 
+function SPatches.Log(...)
+  MsgC(color_cornflower, "[#] ", color_gray, ..., "\n")
+end
+
 if SERVER then
   AddCSLuaFile("patches/sh_patches.lua")
   AddCSLuaFile("patches/cl_patches.lua")
 
   include("patches/sh_patches.lua")
-  include("patches/cl_patches.lua")
   include("patches/sv_patches.lua")
+  include("patches/cl_patches.lua")
 end
 
 if CLIENT then
   include("patches/sh_patches.lua")
   include("patches/cl_patches.lua")
-end
-
-function SPatches.Log(...)
-  MsgC(color_cornflower, "[#] ", color_gray, ..., "\n")
 end
